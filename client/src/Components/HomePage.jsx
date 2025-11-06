c15b350ea63779dcc35c857f1b9b5030a05d5fae
Aditya <adityathakur00199.8@gmail.com>
2025-11-06 22:19:38 +0530

    regsiter form done

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -41,7 +41,7 @@ function HomePage() {
 
         <div className='w-full flex justify-between items-center p-20 gap-10 mt-20'>
           <img 
-            src="../public/photo2.png" 
+            src="/photo2.png" 
             alt="photo-2" 
             className='w-[800px] h-[550px] object-cover hover:scale-110 transition-transform duration-500 ease-in-out cursor-pointer rounded-2xl' 
           />
@@ -64,7 +64,7 @@ function HomePage() {
         <footer className="w-full bg-neutral-900 text-white py-20 mt-16 ">
           <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row items-center justify-between gap-6">
             <div className="flex items-center gap-3">
-              <img src="../public/logo.png" alt="logo" className="w-10 h-10" />
+              <img src="/logo.png" alt="logo" className="w-10 h-10" />
               <div>
                 <h4 className="text-lg font-semibold">Accelerate</h4>
                 <p className="text-sm text-neutral-300">Track Ideas - Fast | Effortless | Secure.</p>
--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -8,26 +8,30 @@ function Register() {
     }
 
   return (
-    <div className='w-full h-screen bg-red-300 flex justify-center items-center'>
-        <div className=' flex justify-between items-center bg-white p-7 w-full max-w-6xl rounded-2xl h-3/4'>
+    <div className='w-full h-screen bg-blue-400 flex justify-center items-center'>
+        <div className=' flex justify-between bg-white items-center p-7 w-full max-w-7xl rounded-2xl h-[90%] gap-6'>
             <div className="relative z-10">
                     <img
                     src="/Leftt.png"
                     alt="Left Decoration"
-                    className="w-64 h-auto -mr-20 z-10 relative scale-400"
+                    className="relative scale-250 w-[180px] h-[250px] ml-28"
                     />
             </div>
-            <form action="submit" className='bg-neutral-200 p-10 flex flex-col items-center justify-center rounded-2xl h-full ' onSubmit={handleRegsiter} >
-                <input type="text" placeholder='Username' />
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <button type='submit'>Register</button>
+            <form action="submit" className='bg-neutral-200 p-10 flex flex-col items-center justify-center rounded-2xl h-full w-sm gap-7' onSubmit={handleRegsiter} >
+                <div className='flex flex-col'>
+                    <div className='p-4 w-full text-center text-4xl font-["Playfair_Display"] tracking-tight text-neutral-700 font-bold'>Track Your Ideas</div>
+                    <div className='text-center'>Please register yourself</div>
+                </div>
+                <input type="text" placeholder='Username' className='p-4 w-full border-2 border-neutral-400 rounded-2xl outline-1 outline-neutral-100' />
+                <input type="email" placeholder='Email' className='p-4 w-full border-2 border-neutral-400 rounded-2xl outline-1 outline-neutral-100' />
+                <input type="password" placeholder='Password' className='p-4 w-full border-2 border-neutral-400 rounded-2xl outline-1 outline-neutral-100' />
+                <button type='submit' className='bg-sky-400 w-full p-4 rounded-full cursor-pointer hover:bg-sky-300 transition-all duration-200 text-xl font-["Montserrat"] font-medium text-white'>Register</button>
             </form>
        <div className="relative z-10">
                     <img
-                    src="/registerright.png"
+                    src="/Rightt.png"
                     alt="Left Decoration"
-                    className="w-64 h-auto -mr-20 z-10 relative"
+                    className="w-[200px] h-auto z-10 relative scale-140 mr-20 mt-40"
                     />
             </div>
         </div>
