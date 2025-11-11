57a35f181b643906b734ca886af3b2b6a3d297f4
Aditya <adityathakur00199.8@gmail.com>
2025-11-11 19:45:10 +0530

    outline done

--- client/src/Components/Progress.jsx
+++ client/src/Components/Progress.jsx
@@ -7,7 +7,7 @@ function Progress() {
 
   return(
     <div className='w-full h-screen'>
-      <div className='flex items-center'>
+      <div className='flex items-center h-full'>
         <div className='w-1/6 h-screen flex flex-col justify-between border-r-2 border-neutral-500'>
         <div className='flex flex-col gap-8 py-8 h-auto justify-center items-start'>
           <button onClick={()=> navigate("/")} className='flex gap-3 items-center px-5 cursor-pointer'>
@@ -120,7 +120,7 @@ function Progress() {
         </button>
       </div>
 
-        <div className='w-5/6 h-screen'>
+        <div className='w-5/6 h-screen flex flex-col'>
         <div className='flex  border-b-neutral-500 border-b-2 justify-between items-center py-5 '>
           <div></div> {/* this is the navabar part */}
           <div className='flex justify-center items-center gap-4 px-4'>
@@ -151,14 +151,16 @@ function Progress() {
           </div>
         </div>
 
-        <div className='w-full h-[90%]'>
           <div className='text-neutral-700 w-full'>
-            <div className='ml-4 mt-2'>
+            <div className='px-4 py-2'>
                 <h1 className='text-2xl font-semibold font-["Playfair_Display"]'>Progress</h1>
                 <p className='font-["Montserrat"] tracking-tight '>Your progress is visible here</p>
             </div>
             </div>
-        </div>
+
+            <div className='flex-1 flex justify-center items-center'>
+              <p>this is the body of the progress.</p>
+            </div>
         </div>
       </div>
     </div>
