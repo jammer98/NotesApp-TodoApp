b41385daf909255b14b76674ea384d82161b48d1
Aditya <adityathakur00199.8@gmail.com>
2025-11-12 16:36:27 +0530

    for now done with the dashboard

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -118,7 +118,7 @@ function Dashboard() {
         </button>
       </div>
       <div className='w-5/6 h-screen flex flex-col'>
-        <div className='flex border-b-neutral-500 border-b-2 justify-between items-center py-5 h-auto bg-white'>
+        <div className='flex border-b-neutral-500 border-b-2 justify-between items-center py-5 bg-white'>
           <div></div>
           <div className='flex justify-center items-center gap-4 px-4'>
             <button className='cursor-pointer'>
@@ -147,13 +147,15 @@ function Dashboard() {
           </button>
           </div>
         </div>
+
+  <div className='overflow-x-auto'>
           <div className='text-neutral-700 w-full'>
             <div className='flex items-center justify-between px-4 py-2' >
               <div>
                 <h1 className='text-2xl font-semibold font-["Playfair_Display"]'>DashBoard</h1>
                 <p className='font-["Montserrat"] tracking-tight '>Create your ideas and work on it and track your progress</p>
               </div>
-              <button onClick={() => navigate("/AddNewIdea")} className='flex items-center gap-3 bg-neutral-300 text-neutral-800 rounded-xl px-3 py-2 cursor-pointer hover:bg-neutral-200'>
+              <button onClick={() => navigate("/AddNewIdea")} className='flex items-center gap-3 bg-neutral-100 text-neutral-800 rounded-xl px-3 py-2 cursor-pointer hover:bg-neutral-50'>
                   <svg xmlns="http://www.w3.org/2000/svg" 
                   fill="none" 
                   viewBox="0 0 24 24" 
@@ -168,8 +170,7 @@ function Dashboard() {
               </button>
             </div>
             </div>
-          <div className='overflow-x-auto'>
-                <div className='p-6 overflow-y-auto'>
+            <div className='p-6 overflow-y-auto'>
               <div className='flex w-full items-stretch gap-4 justify-between'>
                  <div className='flex-1 bg-neutral-100 rounded-2xl p-6 flex flex-col justify-center items-center'>
                     <p className='text-lg opacity-90 font-[""]'>Total Ideas</p>
@@ -198,7 +199,7 @@ function Dashboard() {
                       class="size-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                       </svg>
-                      <h1>5</h1>
+                      <h1>2</h1>
                     </div>
                   </div>
                    <div className='flex-1 bg-green-300 rounded-2xl p-6 flex flex-col justify-center items-center'>
@@ -212,7 +213,7 @@ function Dashboard() {
                       class="size-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
                       </svg>
-                      <h1>4</h1>
+                      <h1>1</h1>
                     </div>
                   </div>
                   <div className='flex-1 bg-sky-300 rounded-2xl p-6 flex flex-col justify-center items-center'>
@@ -228,7 +229,7 @@ function Dashboard() {
                       stroke-linejoin="round" 
                       d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                       </svg>
-                      <h1>3</h1>
+                      <h1>1</h1>
                     </div>
                   </div>
               </div>
@@ -281,7 +282,7 @@ function Dashboard() {
                 </div>
               </div>
             </div>
-          </div>
+  </div>
         </div>
       </div>
     </div>
