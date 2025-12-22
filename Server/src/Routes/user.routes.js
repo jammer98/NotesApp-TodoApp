63f844a74262576e3b9c241159d11e971a002a32
Aditya <adityathakur00199.8@gmail.com>
2025-12-22 20:39:27 +0530

    added routes

--- Server/src/Routes/user.routes.js
+++ Server/src/Routes/user.routes.js
@@ -4,5 +4,9 @@ import express from "express";
 const userrouter = express.Router();
 userrouter.post("/register", RegsiterUser);
 userrouter.post("/login", LoginUser);
+userrouter.get("/allideas",getallideas);
+userrouter.post("/addnewideas",addnewideas);
+userrouter.get("/getuser/:id",getuser);
+
 
 export default userrouter;
