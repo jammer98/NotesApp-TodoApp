4d6d57ddafe7ddf4799411740d24406b2ecbb7ea
Aditya <adityathakur00199.8@gmail.com>
2026-04-22 14:09:52 +0530

    donw

--- Server/src/Middlewares/auth.middleware.js
+++ Server/src/Middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const authMiddleware = (req,res,next)=>{
 
     try {
-        const token =  req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
+        const token =  req.cookies?.accessToken;
 
         if(!token){
             return res.status(401).json({ message: "Unauthorized" });
--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   return (
     <div className='w-full h-screen flex'>
       {/* Sidebar */}
-      <div className='w-1/6 h-screen flex flex-col justify-between border-r-2 border-neutral-500 bg-red-400 px-4'>
+      <div className='w-1/6 h-screen flex flex-col justify-between border-r-2 border-neutral-500 px-4'>
         <div className='flex flex-col gap-8 py-8 h-auto justify-center items-start'>
           <button onClick={() => navigate("/")} className='flex gap-3 items-center px-5 cursor-pointer'>
             <img src="../public/logo.png" alt="logo" className="w-10 h-10 rounded-full" />
@@ -78,8 +78,8 @@ function Dashboard() {
             </button>
           </div>
         </div>
-        <button onClick={() => navigate('/User')} className='w-full h-auto py-5 px-3 flex items-center hover:bg-neutral-200 cursor-pointer'>
-          <div className='px-5'>
+        <button onClick={() => navigate('/User')} className='w-full h-auto flex items-center hover:bg-neutral-200 cursor-pointer'>
+          <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             </svg>
